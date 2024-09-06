af8ae4d59ead53add751ab8c6e6eced8c0fc7625
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-06 10:17:26 +0200

    6 - extra space anew

--- debugging/tic.py
+++ debugging/tic.py
@@ -3,7 +3,7 @@ def print_board(board):
     for row in board:
         print(" ", end="")
         print(" | ".join(row))
-        print("__  " * 3)
+        print("__   " * 3)
 
 def check_winner(board):
     for row in board:
