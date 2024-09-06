017a3e7b7dbc98dea3fe8437dfd76b4594b165a9
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-06 09:20:29 +0200

    2 - first spaces

--- debugging/mines.py
+++ debugging/mines.py
@@ -17,9 +17,9 @@ class Minesweeper:
 
 	def print_board(self, reveal=False):
 		clear_screen()
-		print(' ' + ' '.join(f'{i:2}' for i in range(self.width)))
+		print('   ' + ' '.join(f'{i:2}' for i in range(self.width)))
 		for y in range(self.height):
-			print(f'{y:2}', end=' ')
+			print(f'{y:2}', end='  ')
 			for x in range(self.width):
 				if reveal or self.revealed[y][x]:
 					if (y * self.width + x) in self.mines:
